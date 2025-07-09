3175787babc7cd94e5acd51bf7b9d3aac6839e3f
runer0101 <ccamachod6@gmail.com>
2025-07-09 04:58:05 -0400

    error fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 
-export default {
+export default defineConfig({
   base: '/project-english2/', 
   plugins: [vue()],
   resolve: {
@@ -9,4 +10,4 @@ export default {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}
+})
